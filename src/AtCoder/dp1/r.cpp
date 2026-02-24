bc9fc0983e59c0410d14b651cb75f43965180d65
Aeyone <A_Eyone@163.com>
2026-02-24 10:59:35 +0800

    2.23

---
+++ src/AtCoder/dp1/r.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3fLL
+
+const int MOD = 1e9 + 7;
+
+struct Mat {
+    int n, m;
+    i64 a[60][60] = {};
+
+    Mat() {}
+
+    Mat(int n, int m) {
+        init(n, m);
+    }
+
+    void init(int n, int m) {
+        this->n = n;
+        this->m = m;
+    }
+
+    void I() {
+        memset(a, 0, sizeof(a));
+        for (int i = 0; i < n; i++) {
+            a[i][i] = 1;
+        }
+    }
+
+    Mat operator*(const Mat& T) const {
+        Mat res(n, T.m);
+        int r;
+        for (int i = 0; i < n; i ++) {
+            for (int k = 0; k < T.m; k ++) {
+                r = a[i][k];
+                for (int j = 0; j < m; j ++) {
+                	res.a[i][j] += T.a[k][j] * r;
+                	res.a[i][j] %= MOD;
+                }
+            }
+        }
+        return res;
+    }
+
+    Mat operator^(i64 b) const {
+        Mat res(n, m);
+        res.I();
+        Mat base = *this;
+        while (b) {
+            if (b & 1) {
+                res = res * base;
+            }
+            base = base * base;
+            b >>= 1;
+        }
+        return res;
+    }
+};
+
+void solve() {
+	i64 n, k;
+	cin >> n >> k;
+	Mat mat(n, n);
+	for (int i = 0; i < n; i ++) {
+		for (int j = 0; j < n; j ++) {
+			cin >> mat.a[i][j];
+		}
+	}
+	mat = mat ^ k;
+	i64 ans = 0;
+	for (int i = 0; i < n; i ++) {
+		for (int j = 0; j < n; j ++) {
+			ans = (ans + mat.a[i][j]) % MOD;
+		}
+	}
+	cout << ans << '\n';
+}
+
+signed main() {
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(10);
+	int _ = 1;
+	while (_ --) {
+		solve();
+	}
+}
---
+++ src/AtCoder/dp1/s.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3fLL
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+	string s;
+	cin >> s;
+	int d;
+	cin >> d;
+	int n = s.size();
+
+	reverse(s.begin(), s.end());
+	s = " " + s;
+
+	vector dp(n + 1, vector<i64>(d));// dp[i][j]为前i个数字任意组成的和的模d为j的方案数
+	dp[0][0] = 1;
+
+	for (int i = 1; i < n; i ++) {
+		for (int j = 0; j < d; j ++) {
+			for (int num = 0; num < 10; num ++) {
+				dp[i][j] = (dp[i][j] + dp[i - 1][(d + j - (num % d)) % d]) % MOD;
+			}
+		}
+	}
+
+	i64 ans = MOD - 1; //去除全0
+
+	int cur = 0;
+	for (int i = n; i >= 1; i --) {
+		int u = s[i] - '0';
+		for (int num = 0; num < u; num ++) {
+			ans = (ans + dp[i - 1][(2 * d - cur - (num % d)) % d]) % MOD;
+		}
+		cur = (cur + u) % d;
+	}
+
+	cout << ans + (cur == 0) << '\n';
+
+}
+
+signed main() {
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(10);
+	int _ = 1;
+	while (_ --) {
+		solve();
+	}
+}
