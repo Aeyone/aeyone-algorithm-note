6fc3a4c0b08d6defd088ad02aed5195dc81fc274
Aeyone <A_Eyone@163.com>
2026-05-25 17:25:52 +0800

    123

--- src/XCPC/The 2025 ICPC China Zhejiang Province Programming Contest (22nd)/d.cpp
+++ src/XCPC/The 2025 ICPC China Zhejiang Province Programming Contest (22nd)/d.cpp
@@ -13,7 +13,32 @@ using u128 = unsigned __int128;
 const int MOD = 998244353;
 
 void solve() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 0; i <= n; i ++) {
+        cin >> a[i];
+    }
+    string s;
+    int cur = 0;
+    while (1) {
+        if (cur - 1 >= 0 && a[cur - 1] > 1) {
+            a[-- cur] --;
+            s += "L";
+        }else if (cur + 1 <= n && a[cur + 1] > 1) {
+            a[++ cur] --;
+            s += "R";
+        } else {
+            break;
+        }
+    }
+    if (cur != n - 1 || ranges::count(a, 1) != a.size()) {
+        cout << "Impossible" << '\n';
+        return;
+    }
+    s += "R";
+    s = s + string(n, 'L');
+    cout << s << '\n';
 }
 
 int main() {
