5a296dea954972beb11f467765cb6c5b9b7d6e5f
Aeyone <A_Eyone@163.com>
2026-08-07 00:47:02 +0800

    Modify template

---
+++ src/test/洛谷/网络流24题/P2756.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3fLL
+
+const int MOD = 998244353;
+
+/*
+e保存残量网络中的所有边。
+对于e[i]：
+i为边编号，to是目标节点的编号，cap为当前残量容量，不保存边的起点
+
+建图后的每一轮：
+1、bfs找出当前所有的最短增广路，每次只走cap > 0的边，并且更新出dep数组，dep[i]为当前轮bfs时，从s走到i需要走的最少边数
+
+2、dfs一次性尽量跑完这些最短增广路，每次只走满足dep[v] = dep[u] + 1的边，并且更新出正反边的残量，cur[u]表示当前bfs分层阶段中，节点u的邻接表g[u]已经枚举到哪个位置，这用于保证在当前轮不会重复跑相同的路径。
+
+dfs(u,t,lim)为当前轮允许最多从u向汇点t输送lim单位流的条件下，实际送出了多少流量。
+直到bfs从当前残量网络的s，跑不到汇点t时，停止轮次，所有轮dfs的总和即为整张图的最大流。
+*/
+
+template<class T>
+struct Dinic {
+	struct Edge {
+		int to;
+		T cap;
+		Edge(int to, T cap) : to(to), cap(cap) {}
+	};
+
+	int n;
+	vector<Edge> e;
+	vector<vector<int>> g;
+	vector<int> cur, dep;
+
+    Dinic() {}
+    Dinic(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        this->n = n;
+        e.clear();
+        g.assign(n, {});
+        cur.resize(n);
+        dep.resize(n);
+    }
+
+	bool bfs(int s, int t) {
+		dep.assign(n, -1);
+		queue<int> q;
+		dep[s] = 0;
+		q.push(s);
+		while (q.size()) {
+			const int u = q.front();
+			q.pop();
+			for (int i : g[u]) {
+				auto [v, c] = e[i];
+				if (c > 0 && dep[v] == -1) { 
+					dep[v] = dep[u] + 1;
+					if (v == t) return true;
+					q.push(v);
+				}
+			}
+		}
+		return false;
+	}
+
+	T dfs (int u, int t, T lim) {
+		if (u == t) return lim;
+		auto r = lim; // r为目前还能走多少流量
+		for (int &i = cur[u]; i < g[u].size(); i ++) {
+			const int j = g[u][i];
+			auto [v, c] = e[j];
+			if (c > 0 && dep[v] == dep[u] + 1) {
+				auto a = dfs(v, t, min(r, c));
+				e[j].cap -= a;
+				e[j ^ 1].cap += a;
+				r -= a;
+				if (r == 0) return lim;
+			}
+		}
+		return lim - r;
+	}
+
+	void addEdge(int u, int v, T c) {
+        g[u].push_back(e.size());
+        e.emplace_back(v, c);
+        g[v].push_back(e.size());
+        e.emplace_back(u, 0);
+    }
+
+	T flow(int s, int t) {
+		T ans = 0;
+		while (bfs(s, t)) {
+			cur.assign(n, 0);
+			ans += dfs(s, t, numeric_limits<T>::max());
+		}
+		return ans;
+	}
+
+    struct _Edge {
+        int u, v;
+        T cap;
+        T flow;
+    };
+
+    vector<_Edge> edges() {
+        vector<_Edge> a;
+        for (int i = 0; i < e.size(); i += 2) {
+            _Edge x;
+            x.u = e[i + 1].to;
+            x.v = e[i].to;
+            x.cap = e[i].cap + e[i + 1].cap;
+            x.flow = e[i + 1].cap;
+            a.push_back(x);
+        }
+        return a;
+    }
+};
+
+
+
+void solve() {
+	int n, m;
+	cin >> m >> n;
+
+	int s = 0, t = n + 1;
+	Dinic<int> d(t + 1);
+	for (int i = 1; i <= m; i ++) {
+		d.addEdge(0, i, 1);
+	}
+	for (int i = m + 1; i <= n; i ++) {
+		d.addEdge(i, t, 1);
+	}
+	while (1) {
+		int u, v;
+		cin >> u >> v;
+		if (u == -1) break;
+		d.addEdge(u, v, 1);
+	}
+
+	int ans = d.flow(s, t);
+	cout << ans << '\n';
+
+	auto tot = d.edges();
+	for (auto [u, v, c, f] : tot) {
+		if (f > 0 && u != s && v != t) {
+			cout << u << ' ' << v << '\n';
+		}
+	}
+
+}
+
+signed main() {
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(10);
+	int _ = 1;
+	// cin >> _;
+	while (_ --) {
+		solve();
+	}
+}
